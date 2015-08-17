c34097bf21e34577703d42d4b5d81dc3a64d00ba
gustawho <gustawho@gmail.com>
2015-08-17 02:53:59 -0600

    Added Dark variant

--- src/spotify-kde-integration.sh
+++ src/spotify-kde-integration.sh
@@ -1,86 +1,96 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 set -e
 
-#############################################################################
 ## Helpers & Config
-#############################################################################
 
 msg() {
-    tput setab 2 # green bg
-    tput setaf 7 # white text
-    echo ">>> $1"
-    tput sgr 0
-    sleep 1
+  tput setaf 4
+  echo ">>> $1"
+  tput sgr 0
+  sleep 1
 }
 
 tmp_dir="/tmp/fsi-$(date +%s)"
 
-#############################################################################
 ## Main Script
-#############################################################################
 
 main() {
-    msg "Entering temporary directory..."
-    mkdir "$tmp_dir"
-    cd "$tmp_dir"
 
-    msg "Making a copy of resources.zip..."
-    cp /opt/spotify/spotify-client/Data/resources.zip resources_old.zip
-    unzip resources_old.zip -d resources_old/
+  msg "Entering temporary directory..."
+  mkdir "$tmp_dir"
+  cd "$tmp_dir"
 
+  msg "Backing up resources.zip..."
+  cp /opt/spotify/spotify-client/Data/resources.zip resources_old.zip
+  unzip resources_old.zip -d resources_old/
+
+  echo "There are two variants for the tray icon, one for each Breeze style (light and dark),"
+  read -p "Which one should I use, (D)ark or (L)ight?" $userin
+  if [[ $userin = [dD] ]]; then
     msg "Downloading assets..."
-    wget -O skin.xml https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/skin.xml
-    # Icons
-    wget -O spotify-linux-16.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/spotify-linux-16.png
-    wget -O spotify-linux-22.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/spotify-linux-22.png
-    wget -O spotify-linux-24.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/spotify-linux-24.png
-    wget -O spotify-linux-32.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/spotify-linux-32.png
-    wget -O spotify-linux-48.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/spotify-linux-48.png
-    wget -O spotify-linux-64.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/spotify-linux-64.png
-    wget -O spotify-linux-128.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/spotify-linux-128.png
-    wget -O spotify-linux-256.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/spotify-linux-256.png
-    wget -O spotify-linux-512.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/spotify-linux-512.png
-    # Images
-    wget -O images.zip https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/images.zip
-    # Skin
-    wget -O skin.xml https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/skin.xml
-    
-    msg "Extracting ZIP..."
-    unzip images.zip -d images/
+    wget -O spotify-linux-16.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/dark/spotify-linux-16.png
+    wget -O spotify-linux-22.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/dark/spotify-linux-22.png
+    wget -O spotify-linux-24.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/dark/spotify-linux-24.png
+    wget -O spotify-linux-32.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/dark/spotify-linux-32.png
+    wget -O spotify-linux-48.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/dark/spotify-linux-48.png
+    wget -O spotify-linux-64.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/dark/spotify-linux-64.png
+    wget -O spotify-linux-128.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/dark/spotify-linux-128.png
+    wget -O spotify-linux-256.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/dark/spotify-linux-256.png
+    wget -O spotify-linux-512.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/dark/spotify-linux-512.png
+  else
+    if [[ $userin = [lL] ]]; then
+      msg "Downloading assets..."
+      wget -O spotify-linux-16.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/light/spotify-linux-16.png
+      wget -O spotify-linux-22.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/light/spotify-linux-22.png
+      wget -O spotify-linux-24.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/light/spotify-linux-24.png
+      wget -O spotify-linux-32.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/light/spotify-linux-32.png
+      wget -O spotify-linux-48.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/light/spotify-linux-48.png
+      wget -O spotify-linux-64.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/light/spotify-linux-64.png
+      wget -O spotify-linux-128.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/light/spotify-linux-128.png
+      wget -O spotify-linux-256.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/light/spotify-linux-256.png
+      wget -O spotify-linux-512.png https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/icons/light/spotify-linux-512.png
+    else
+      echo "You didn't give a valid option. Press D for Dark or L for Light."
+    fi
+  fi
 
-    msg "Applying new skin..."
-    cp spotify-linux-16.png resources_old/_linux/spotify-linux-16.png
-    cp spotify-linux-22.png resources_old/_linux/spotify-linux-22.png
-    cp spotify-linux-24.png resources_old/_linux/spotify-linux-24.png
-    cp spotify-linux-32.png resources_old/_linux/spotify-linux-32.png
-    cp spotify-linux-48.png resources_old/_linux/spotify-linux-48.png
-    cp spotify-linux-64.png resources_old/_linux/spotify-linux-64.png
-    cp spotify-linux-128.png resources_old/_linux/spotify-linux-128.png
-    cp spotify-linux-256.png resources_old/_linux/spotify-linux-256.png
-    cp spotify-linux-512.png resources_old/_linux/spotify-linux-512.png
-    cp -f images/*.png resources_old/
-    cp -f skin.xml resources_old/skin.xml
+  # Images
+  wget -O images.zip https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/images.zip
+  # Skin
+  wget -O skin.xml https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/skin.xml
+  
+  msg "Extracting ZIP..."
+  unzip images.zip -d images/
 
-    msg "Packaging resources.zip..."
-    cd resources_old/
-    zip -r resources_patched.zip .
-    cd ..
-    mv resources_old/resources_patched.zip .
+  msg "Applying new skin..."
+  cp spotify-linux-16.png resources_old/_linux/spotify-linux-16.png
+  cp spotify-linux-22.png resources_old/_linux/spotify-linux-22.png
+  cp spotify-linux-24.png resources_old/_linux/spotify-linux-24.png
+  cp spotify-linux-32.png resources_old/_linux/spotify-linux-32.png
+  cp spotify-linux-48.png resources_old/_linux/spotify-linux-48.png
+  cp spotify-linux-64.png resources_old/_linux/spotify-linux-64.png
+  cp spotify-linux-128.png resources_old/_linux/spotify-linux-128.png
+  cp spotify-linux-256.png resources_old/_linux/spotify-linux-256.png
+  cp spotify-linux-512.png resources_old/_linux/spotify-linux-512.png
+  cp -f images/*.png resources_old/
+  cp -f skin.xml resources_old/skin.xml
 
-    msg "Replacing current resources.zip..."
-    sudo cp resources_patched.zip /opt/spotify/spotify-client/Data/resources.zip
+  msg "Packaging resources.zip..."
+  cd resources_old/
+  zip -r resources_patched.zip .
+  cd ..
+  mv resources_old/resources_patched.zip .
 
-    msg "Cleaning up..."
-    rm -rf "$tmp_dir"
+  msg "Replacing current resources.zip..."
+  sudo cp resources_patched.zip /opt/spotify/spotify-client/Data/resources.zip
 
-    msg "The Spotify icon has been replaced successfuly!"
-    msg "Start Spotify again to check it out."
-    sleep 2
+  msg "Cleaning up..."
+  rm -rf "$tmp_dir"
+
+  msg "Skin successfuly applied!"
+  msg "Restart Spotify to check it out."
+  sleep 2
 }
-
-#############################################################################
-## Bootstrap it
-#############################################################################
 
 main "$@"
