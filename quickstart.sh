589a7f18b366a50bf05a1782a9339af2339d9057
gustawho <gustawho@gmail.com>
2015-08-17 04:37:28 -0600

    Script cleanup

--- quickstart.sh
+++ quickstart.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-wget -O spotify-kde-integration.sh https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/spotify-kde-integration.sh
+echo -e "\e[1m\e[32m==> \e[39mDownloading main script...\e[0m"
+wget -O spotify-kde-integration.sh https://raw.githubusercontent.com/gustawho/spotify-kde-integration/master/src/spotify-kde-integration.sh 2> /dev/null
 
 chmod +x spotify-kde-integration.sh
 
+echo -e "\e[1m\e[32m==> \e[39mInstalling script..."
 sudo mv spotify-kde-integration.sh /usr/bin/spotify-kde-integration
 
-spotify-kde-integration
+sh /usr/bin/spotify-kde-integration
